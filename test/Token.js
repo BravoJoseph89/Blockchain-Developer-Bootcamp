e8442715037cf3bfede27125fabfdc47bfbd926e
Joe Bravo <bravojoseph89@gmail.com>
2025-06-14 20:42:46 -0700

    5. Token Transfers

--- test/Token.js
+++ test/Token.js
@@ -6,18 +6,18 @@ const tokens = (n) => {
 }
 
 describe('Token', () => {
-	let token, accounts, deployer
+	let token, accounts, deployer, receiver
 
-	
 	beforeEach(async () => {
 		const Token = await ethers.getContractFactory('Token')
 		token = await Token.deploy('Dapp University','DAPP', '1000000')
 
 		accounts = await ethers.getSigners()
 		deployer = accounts[0]
+		receiver = accounts[1]
 	})
 
-	describe('Deployoment', () => {
+	describe('Deployment', () => {
 		const name = 'Dapp University'
 		const symbol = 'DAPP'
 		const decimals = '18'
@@ -38,10 +38,52 @@ describe('Token', () => {
 		it('has correct total supply', async () => { 
 			expect(await token.totalSupply()).to.equal(totalSupply)
 		})
+		
 		it('assigns total supply to deployer', async () => { 
 			expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
 		})
+	
+	})
 
+
+	describe('Sending Tokens', () => {
+		let amount, transaction, result
+
+		describe('Success', () => {
+
+			beforeEach(async () => {
+				amount = tokens(100)
+				transaction = await token.connect(deployer).transfer(receiver.address, amount)
+				result = await transaction.wait()
+			})
+	
+			it('transfers token balances', async () => {
+				expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+				expect(await token.balanceOf(receiver.address)).to.equal(amount)
+			})	
+	
+			it('emits a transfer event', async () => {
+				const event = result.events[0]
+				expect(event.event).to.equal('transfer')
+	
+				const args = event.args
+				expect(args.from).to.equal(deployer.address)
+				expect(args.to).to.equal(receiver.address)
+				expect(args.value).to.equal(amount)
+			})
+		})
+
+		describe('Failure', () => {
+			it('rejects insufficient balances', async () => {
+				const invalidAmount = tokens(100000000)				
+				await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+			})
+
+			it('rejects invalid recipients', async () => {
+				const invalidRecipient = '0x0000000000000000000000000000000000000000'
+				await expect(token.connect(deployer).transfer(invalidRecipient, tokens(100))).to.be.reverted
+			})
+		})
 	})
 
 })
