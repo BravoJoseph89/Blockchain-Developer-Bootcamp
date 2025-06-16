b08fad2cf29cb66a82438b4e29ad80d332240f3c
Joe Bravo <bravojoseph89@gmail.com>
2025-06-16 14:19:29 -0700

    git commit -m "Token Deployment"

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
+const privateKey = process.env.PRIVATE_KEYS || ""
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost:{}
+    localhost:{},
+    sepolia:{
+      url:  `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: privateKey.split(",")
+    }
   }
 };
